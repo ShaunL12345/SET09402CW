d9fd9838b1ebaf959f8ffbcacfda765249853d21
40346200Aidan <40346200@live.napier.ac.uk>
2024-07-07 18:42:15 +0100

    AG - Added base classes for other devs to build from

---
+++ Haulage/BaseClasses/TripHandler/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haulage.BaseClasses.TripHandler
+{
+    public class Item
+    {
+        public Guid itemID;
+        public string description;
+        public bool itemCategory;
+        public bool signedOff;
+        public Item() { }
+
+        public void inspectItem(){
+
+        }
+
+    }
+}
