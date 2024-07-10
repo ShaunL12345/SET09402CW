20580952254990519df54388cf2f469aeac016e3
Liam Morris <123487506+40081168LJ@users.noreply.github.com>
2024-07-10 13:21:43 +0100

    LJ - Second Commit for Haulage Test Changes.

--- HaulageTests/obj/Debug/net8.0/HaulageTests.AssemblyInfo.cs
+++ HaulageTests/obj/Debug/net8.0/HaulageTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HaulageTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2548e2099aac65f9d4219c67687c218f1a9b4e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4e56dd30af0000cf8ba92711edae1da7d9ec071")]
 [assembly: System.Reflection.AssemblyProductAttribute("HaulageTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HaulageTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
