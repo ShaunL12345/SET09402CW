7b7b899fab54a2842c9d844de0eb98f386451ea0
40346200Aidan <40346200@live.napier.ac.uk>
2024-07-12 14:48:00 +0100

    AG - Made a start on the database tables

--- Haulage/DatabaseSetup.cs
+++ Haulage/DatabaseSetup.cs
@@ -29,14 +29,78 @@ public static class DatabaseSetup
             using (var connection = new SQLiteConnection(GetDatabasePath()))
             {
                 List<string> createTableScripts = new List<string>();
-               
+
                 //Create User Table for Haulage Data;
+
                 createTableScripts.Add(@"
-                CREATE TABLE IF NOT EXISTS users (
+                CREATE TABLE IF NOT EXISTS user (
                 UserID INTEGER PRIMARY KEY AUTOINCREMENT,
                 RoleID INTEGER NOT NULL,
                 FullName TEXT NOT NULL
                  );");
+
+                createTableScripts.Add(@"
+                CREATE TABLE IF NOT EXISTS item (
+                ItemID INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name TEXT NOT NULL,
+                Description TEXT NOT NULL
+                );");
+
+                createTableScripts.Add(@"
+                CREATE TABLE IF NOT EXISTS Trip (
+                TripId INTEGER PRIMARY KEY AUTOINCREMENT,
+                StartLocation TEXT NOT NULL,
+                EndLocation TEXT NOT NULL
+                );");
+
+                createTableScripts.Add(@"
+                CREATE TABLE IF NOT EXISTS Role ( 
+                RoleId INTEGER PRIMARY KEY AUTOINCREMENT,
+                Role TEXT NOT NULL,
+                FullName TEXT NOT NULL
+                );");
+
+                createTableScripts.Add(@"
+                CREATE TABLE IF NOT EXISTS Vehicle (
+                VehicleId INTEGER PRIMARY KEY AUTOINCREMENT,
+                LicensePlate TEXT UNIQUE NOT NULL,
+                Capacity INTEGER NOT NULL,
+                DriverId INTEGER NOT NULL,
+                );");
+
+                createTableScripts.Add(@"
+                CREATE TABLE IF NOT EXISTS TripManifest (
+                ManifestId INTEGER PRIMARY KEY AUTOINCREMENT,
+                TripId INTEGER NOT NULL,
+                PickUpRequest INTEGER NOT NULL,
+                );");
+
+                createTableScripts.Add(@"
+                CREATE TABLE IF NOT EXISTS Bill (
+                BillId INTEGER PRIMARY KEY AUTOINCREMENT,
+                Fullname TEXT NOT NULL,
+                Email TEXT NOT NULL
+                );");
+
+                createTableScripts.Add(@"
+                CREATE TABLE IF NOT EXISTS Expense (
+                ExpenseId INTEGER PRIMARY KEY AUTOINCREMENT,
+                DriverId INTEGER NOT NULL,
+                VehicleId INTEGER NOT NULL
+                );");
+
+                createTableScripts.Add(@"
+                CREATE TABLE IF NOT EXISTS Event (
+                EventId INTEGER PRIMARY KEY AUTOINCREMENT,
+                DriverId INTEGER NOT NULL,
+                );");
+
+                createTableScripts.Add(@"
+                CREATE TABLE IF NOT EXISTS Waypoint (
+                 
+                )");
+
+                
 
                 foreach (string tableScript in createTableScripts) 
                 {
