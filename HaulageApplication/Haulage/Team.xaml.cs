96caf3baac2cdf14747eb30a3d3a780f00c14aae
40346200Aidan <40346200@live.napier.ac.uk>
2024-07-14 10:59:44 +0100

    AG - Team page comments

--- HaulageApplication/Haulage/Team.xaml.cs
+++ HaulageApplication/Haulage/Team.xaml.cs
@@ -1,7 +1,9 @@
 namespace Haulage;
 
+//Class to containg Team page functionality ( inherit from content page)
 public partial class Team : ContentPage
 {
+	//Method to initialize page
 	public Team()
 	{
 		InitializeComponent();
