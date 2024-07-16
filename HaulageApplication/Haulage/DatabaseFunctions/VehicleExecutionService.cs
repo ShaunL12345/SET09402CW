47ed9f0485b93042957a7163d5fea894e5dbd99a
ShaunL12345 <40587305@live.napier.ac.uk>
2024-07-16 23:47:09 +0100

    made changes so database is cleared and re-generated each run

---
+++ HaulageApplication/Haulage/DatabaseFunctions/VehicleExecutionService.cs
@@ -0,0 +1,33 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Haulage.BaseClasses;
+using Haulage.BaseClasses.Accounting;
+
+namespace Haulage.DatabaseFunctions
+{
+    class VehicleExecutionService
+    {
+        public List<Vehicle> getVehicles() 
+        {
+            var vehicles = new List<Vehicle>();
+            var sql = "SELECT [VehicleId]     " +
+                ",[LicensePlate]     " +
+                ",[Capacity]      " +
+                ",[DriverId]  " +
+                ",[Status]  " +
+                "FROM [Vehicle];";
+
+            using (var connection = new SQLiteConnection(DatabaseSetup.GetDatabasePath()))
+            {
+                var command = new SQLite.SQLiteCommand(connection);
+                command.CommandText = sql;
+                vehicles = command.ExecuteQuery<Vehicle>();
+            }
+            return vehicles;
+        }
+    }
+}
