a74faa7bc78ebef9eaf3a2d24f14bf6ad7da68e7
40346200Aidan <40346200@live.napier.ac.uk>
2024-07-16 09:51:03 +0100

    AG -Trip manifest comments

--- HaulageApplication/Haulage/BaseClasses/TripHandler/TripManifest.cs
+++ HaulageApplication/Haulage/BaseClasses/TripHandler/TripManifest.cs
@@ -6,14 +6,17 @@ using System.Threading.Tasks;
 
 namespace Haulage.BaseClasses.TripHandler
 {
+    //Class to contain trip manifest functionality 
     public class TripManifest
     {
+        //varibles to contain attributes related to trip manifest
         public Guid manifestId;
         public Guid tripId;
         public int pickUpRequest;
 
         public TripManifest() { }
 
+        //method to allow drivers / administrators to inspect trip manifest
         public void inspectItem()
         {
 
