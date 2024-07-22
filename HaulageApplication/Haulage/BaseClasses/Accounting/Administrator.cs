0cdf96396b254f31fd2f96c5aa04af7b6501020d
Liam Morris <123487506+40081168LJ@users.noreply.github.com>
2024-07-22 21:21:09 +0100

    LJ - Adding Admin Manifest options, Add Item, Update Item.

--- HaulageApplication/Haulage/BaseClasses/Accounting/Administrator.cs
+++ HaulageApplication/Haulage/BaseClasses/Accounting/Administrator.cs
@@ -35,6 +35,25 @@ namespace Haulage.BaseClasses.Accounting
 
         }
 
+        public void AddManifestItem(TripManifest manifest, ManifestItem item)
+        {
+            manifest.Items.Add(item);
+        }
+
+        public void RemoveManifestItem(TripManifest manifest, ManifestItem item)
+        {
+            manifest.Items.Remove(item);
+        }
+
+        public void UpdateManifestItem(TripManifest manifest, ManifestItem oldItem, ManifestItem newItem)
+        {
+            int index = manifest.Items.IndexOf(oldItem);
+            if (index != -1)
+            {
+                manifest.Items[index] = newItem;
+            }
+        }
+
         public void planRoutes()
         {
 
@@ -51,5 +70,18 @@ namespace Haulage.BaseClasses.Accounting
         }
     }
 
+    public class TripManifest
+    {
+        public int ManifestId { get; set; }
+        public int TripId { get; set; }
+        public List<ManifestItem> Items { get; set; } = new List<ManifestItem>();
+    }
+
+    public class ManifestItem
+    {
+        public int ItemId { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; } // Fragile, Dangerous, Other
+    }
 
 }
