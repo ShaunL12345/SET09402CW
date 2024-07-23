6c96382b32fe2be8e21e128af1cedf6cb2d016db
40346200Aidan <40346200@live.napier.ac.uk>
2024-07-23 23:24:21 +0100

    AG - Add employee button working

--- HaulageApplication/Haulage/Administrator Pages/AddEmployee.xaml.cs
+++ HaulageApplication/Haulage/Administrator Pages/AddEmployee.xaml.cs
@@ -1,15 +1,42 @@
 namespace Haulage;
+using SQLite;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 public partial class AddEmployee : ContentPage
 {
-	public AddEmployee()
+   
+   
+    public AddEmployee()
 	{
 		InitializeComponent();
+
+        Button SaveEmployee = FindByName("SaveEmployee") as Button;
+        SaveEmployee.Clicked += SaveEmployee_Clicked;
+
+       
     }
 
     private void SaveEmployee_Clicked(object sender, EventArgs e)
     {
 
+        string employeeID = idEntry.Text;
+        string employeeName = nameEntry.Text;
+        string employeeEmail = emailEntry.Text;
+        string employeeNumber = phoneNumberEntry.Text;
+        string employeeRole = roleEntry.Text;
+        string employeeAddress = addressEntry.Text;
+        string employeeQualification = qualificationEntry.Text;
+
+        var sql = $"INSERT INTO USER VALUES ({employeeID}, '{employeeName}', '{employeeEmail}', '{employeeNumber}', '{employeeRole}', '{employeeAddress}', '{employeeQualification}');";
+            
+        
+        using (var connection = new SQLiteConnection(DatabaseSetup.GetDatabasePath()))
+        {
+            var command = new SQLite.SQLiteCommand(connection);
+            command.CommandText = sql;
+            command.ExecuteQuery<DBNull>();
+        }
 
     }
 }
--- HaulageApplication/Haulage/DatabaseExecutionServices/UserExecutionService.cs
+++ HaulageApplication/Haulage/DatabaseExecutionServices/UserExecutionService.cs
@@ -32,25 +32,5 @@ namespace Haulage.DatabaseExecutionServices
             return drivers;
         }
 
-        public static List<AddEmployee> GetAddDrivers()
-        {
-            var addEmployees = new List<AddEmployee>();
-            var sql = "SELECT [Id]" +
-                ",[Name]    " +
-                ",[Email]     " +
-                ",[PhoneNumber]      " +
-                ",[Role]  " +
-                ",[Address]  " +
-                ",[Qualification] " +
-                "FROM [User];";
-
-            using (var connection = new SQLiteConnection(DatabaseSetup.GetDatabasePath()))
-            {
-                var command = new SQLite.SQLiteCommand(connection);
-                command.CommandText = sql;
-                addEmployees = command.ExecuteQuery<AddEmployee>();
-            }
-            return addEmployees;
-        }
     }
 }
