ab43486c6ef395b6e1bde470f3f98b0845ca33b7
Liam Morris <123487506+40081168LJ@users.noreply.github.com>
2024-07-23 22:08:18 +0100

    LJ - Found issue chanign pages

--- HaulageApplication/Haulage/CustomerPage.xaml.cs
+++ HaulageApplication/Haulage/CustomerPage.xaml.cs
@@ -1,25 +1,30 @@
-namespace Haulage;
 using Haulage.BaseClasses.Accounting;
 using System;
 
+namespace Haulage;
+
 public partial class CustomerPage : ContentPage
 {
-    private Customer _customer;
-    private Account _account;
+    //private Customer _customer;
+    //private Account _account;
 
-    public CustomerPage(Customer customer, Account account)
+    //public CustomerPage(Customer customer, Account account)
+    //{
+    //    InitializeComponent();
+    //    _customer = customer;
+    //    _account = account;
+
+    //    BillingDetailsEntry.Text = _account.BillingDetails;
+    //    ContactDetailsEntry.Text = _account.ContactDetails;
+    //}
+
+    //private void SaveButton_Click(object sender, EventArgs e)
+    //{
+    //    _customer.ManageAccount(_account, BillingDetailsEntry.Text, ContactDetailsEntry.Text);
+    //    DisplayAlert("Success", "Account details updated successfully.", "OK");
+    //}
+    public CustomerPage()
     {
         InitializeComponent();
-        _customer = customer;
-        _account = account;
-
-        BillingDetailsEntry.Text = _account.BillingDetails;
-        ContactDetailsEntry.Text = _account.ContactDetails;
-    }
-
-    private void SaveButton_Click(object sender, EventArgs e)
-    {
-        _customer.ManageAccount(_account, BillingDetailsEntry.Text, ContactDetailsEntry.Text);
-        DisplayAlert("Success", "Account details updated successfully.", "OK");
     }
 }
