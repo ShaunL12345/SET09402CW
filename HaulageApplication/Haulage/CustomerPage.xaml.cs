c73b3a4f67dcb08401adff6a5eb5f725fc5288a6
Liam Morris <123487506+40081168LJ@users.noreply.github.com>
2024-07-23 21:54:18 +0100

    LJ - Trying to add some UI elements for manager accout, issue transition to another page.

--- HaulageApplication/Haulage/CustomerPage.xaml.cs
+++ HaulageApplication/Haulage/CustomerPage.xaml.cs
@@ -1,9 +1,25 @@
 namespace Haulage;
+using Haulage.BaseClasses.Accounting;
+using System;
 
 public partial class CustomerPage : ContentPage
 {
-	public CustomerPage()
-	{
-		InitializeComponent();
-	}
+    private Customer _customer;
+    private Account _account;
+
+    public CustomerPage(Customer customer, Account account)
+    {
+        InitializeComponent();
+        _customer = customer;
+        _account = account;
+
+        BillingDetailsEntry.Text = _account.BillingDetails;
+        ContactDetailsEntry.Text = _account.ContactDetails;
+    }
+
+    private void SaveButton_Click(object sender, EventArgs e)
+    {
+        _customer.ManageAccount(_account, BillingDetailsEntry.Text, ContactDetailsEntry.Text);
+        DisplayAlert("Success", "Account details updated successfully.", "OK");
+    }
 }
