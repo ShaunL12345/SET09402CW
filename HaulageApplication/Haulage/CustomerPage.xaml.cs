4170033a3a469288e37189ddc93c58aaae58c64c
Liam Morris <123487506+40081168LJ@users.noreply.github.com>
2024-07-24 10:55:31 +0100

    LJ - Fixed so Navigation now works and button working.

--- HaulageApplication/Haulage/CustomerPage.xaml.cs
+++ HaulageApplication/Haulage/CustomerPage.xaml.cs
@@ -6,27 +6,35 @@ namespace Haulage;
 
 public partial class CustomerPage : ContentPage
 {
-    //private Customer _customer;
-    //private Account _account;
-
-    //public CustomerPage(Customer customer, Account account)
-    //{
-    //    InitializeComponent();
-    //    _customer = customer;
-    //    _account = account;
-
-    //    // Initialize the entries
-    //    BillingDetailsEntry.Text = account.BillingDetails;
-    //    ContactDetailsEntry.Text = account.ContactDetails;
-    //}
-
-    public void SaveButton_Click(object sender, EventArgs e)
-    {
-        //_customer.ManageAccount(_account, BillingDetailsEntry.Text, ContactDetailsEntry.Text);
-    }
+    private Customer _customer;
+    private Account _account;
 
     public CustomerPage()
     {
         InitializeComponent();
+    }
+
+    public CustomerPage(Customer customer, Account account)
+    {
+        InitializeComponent();
+        _customer = customer ?? throw new ArgumentNullException(nameof(customer));
+        _account = account ?? throw new ArgumentNullException(nameof(account));
+
+        // Initialize the entries
+        BillingDetailsEntry.Text = account.BillingDetails;
+        ContactDetailsEntry.Text = account.ContactDetails;
+    }
+
+    public void SaveButton_Click(object sender, EventArgs e)
+    {
+        if (_customer != null && _account != null)
+        {
+            _customer.ManageAccount(_account, BillingDetailsEntry.Text, ContactDetailsEntry.Text);
+            DisplayAlert("Success", "Account details updated successfully.", "OK");
+        }
+        else
+        {
+            DisplayAlert("Error", "Failed to update account details.", "OK");
+        }
     }
 }
