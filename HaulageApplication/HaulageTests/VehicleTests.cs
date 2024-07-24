415fd8123e89ac17aea991f65bdefda4ed5b0149
ShaunL12345 <40587305@live.napier.ac.uk>
2024-07-24 17:08:29 +0100

    added more tests

--- HaulageApplication/HaulageTests/VehicleTests.cs
+++ HaulageApplication/HaulageTests/VehicleTests.cs
@@ -37,21 +37,32 @@ namespace HaulageTests
         [Fact]
         public void DeleteVehicleFromDatabse()
         {
-            //DatabaseSetup.InitializeDatabase();
-            //var vehicle = new Vehicle() 
-            //{
+            DatabaseSetup.InitializeDatabase();
+            var vehicles = VehicleExecutionService.GetVehicles();
+            var vehicleToRemove = vehicles.First();
+            var initialVehicleCount = vehicles.Count;
+            VehicleExecutionService.DeleteVehicle(vehicleToRemove.VehicleId); 
 
-            //}
-
-            //Assert.True(vehicles.Count > 0, "Did not recieve any records from GetVehicles method");
+            Assert.True(initialVehicleCount == initialVehicleCount-1, "Failed to delete a vehicle record");
         }
         [Fact]
         public void SaveVehicleToDatabse()
         {
             DatabaseSetup.InitializeDatabase();
-            var vehicles = VehicleExecutionService.GetVehicles();
-            Assert.NotNull(vehicles);
-            Assert.True(vehicles.Count > 0, "Did not recieve any records from GetVehicles method");
+            var vehiclesCountBefore = VehicleExecutionService.GetVehicles().Count;
+
+            Vehicle vehicle = new Vehicle()
+            {
+                VehicleId = 132321,
+                tripID = 4321321,
+                Capacity = 123321,
+                DriverId = 321213,
+                LicensePlate = "ASD23HF",
+                Status = Vehicle.StatusType.TestEnum1
+            };
+            VehicleExecutionService.SaveVehicle(vehicle);
+            var vehiclesCountAfter = VehicleExecutionService.GetVehicles().Count;
+            Assert.True(vehiclesCountAfter== vehiclesCountBefore+1, "VehiclesRecord was not saved");
         }
 
         public void Dispose()
