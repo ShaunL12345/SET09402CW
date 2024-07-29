cf8fd8e7c75ace03ff1d04ce487c90359f92cb4b
Liam Morris <123487506+40081168LJ@users.noreply.github.com>
2024-07-29 21:49:17 +0100

    LJ - Updates after merging.

---
+++ HaulageApplication/Haulage/BaseClasses/Accounting/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haulage.BaseClasses.Accounting
+{
+    public class Person
+    {
+        string name, emailAddress;
+        int phoneNumber;
+        public Guid id;
+        enum Role
+        {
+            driver,
+            administrator,
+            customer
+        }
+
+        public Person() { }
+
+    }
+}
