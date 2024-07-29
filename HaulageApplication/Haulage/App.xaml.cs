603232b9ff51a9785f33ae0025912f52861c59d4
40346200Aidan <40346200@live.napier.ac.uk>
2024-07-29 10:37:08 +0100

    AG - Commented out Admin tests for testing WIP

--- HaulageApplication/Haulage/App.xaml.cs
+++ HaulageApplication/Haulage/App.xaml.cs
@@ -8,13 +8,4 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
-
-    protected override Window CreateWindow(IActivationState activationState)
-    {
-        Window window = base.CreateWindow(activationState);
-
-       
-
-        return window;
-    }
 }
