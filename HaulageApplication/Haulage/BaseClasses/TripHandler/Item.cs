911934bc1534d030d26b5a3ba3a977c04e62339a
ShaunL12345 <40587305@live.napier.ac.uk>
2024-08-07 15:20:02 +0100

    updated bindings

--- HaulageApplication/Haulage/BaseClasses/TripHandler/Item.cs
+++ HaulageApplication/Haulage/BaseClasses/TripHandler/Item.cs
@@ -15,17 +15,6 @@ namespace Haulage.BaseClasses.TripHandler
             Dangerous,
             None
         }
-        //public enum RequestStatusType
-        //{
-        //    pickUpRequested,
-        //    EnRouteToPickup,
-        //    PickedUp,
-        //}
-        public Guid itemID;
-        public string description;
-        public ItemCategoryType itemCategory;
-        public bool signedOff;
-        public Item() { }
         public int ItemId { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
